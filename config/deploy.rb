2b1e482c1bad840fcf70521ee1059eeff188a0ba
Kolomoets Oleg <okolomoets@prophonix.com>
2013-12-09 12:00:33 +0200

    Fix deploy.rb and unicorn.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 #require 'bundler/capistrano'  # Add Bundler integration
 require 'capistrano/ext/multistage'
 require 'rvm/capistrano' # This is for working with capistrano
-require 'capistrano-unicorn'
+require "bundler/capistrano"
 
 set :rails_env, "production"
 set :stages, %w(staging production)
@@ -21,7 +21,9 @@ set :repository, "git@github.com:KolomoetsOleg/unicorn_nginx.git"
 set :branch, "master"
 set :deploy_via, :remote_cache    
 
-before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby'
+# before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby'
 
+require 'capistrano-unicorn'
+after 'deploy', 'unicorn:start'
 after 'deploy', 'unicorn:reload'    # app IS NOT preloaded
 after 'deploy', 'unicorn:restart'   # app preloaded
