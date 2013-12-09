042a01a84a05943c50c3adc69a8d78f816dc819c
Kolomoets Oleg <okolomoets@prophonix.com>
2013-12-09 10:35:33 +0200

    Change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 #require 'bundler/capistrano'  # Add Bundler integration
 require 'capistrano/ext/multistage'
 require 'rvm/capistrano' # This is for working with capistrano
+require 'capistrano-unicorn'
 
-set :application, "unicorn_nginx"
 set :rails_env, "production"
 set :stages, %w(staging production)
 set :default_stage, 'production'
@@ -14,30 +14,14 @@ set :use_sudo, false
 set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
 set :unicorn_pid, "#{deploy_to}/tmp/pids/unicorn.pid"
 
-set :rvm_ruby_string, 'ree'
+
 
 set :scm, :git
 set :repository, "git@github.com:KolomoetsOleg/unicorn_nginx.git"
 set :branch, "master"
-set :deploy_via, :remote_cache
+set :deploy_via, :remote_cache    
 
+before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby'
 
-after 'deploy:update_code', :roles => :app do
-  run "rm -f #{current_release}/config/database.yml"
-  run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
-end  
-
-namespace :deploy do
-  # task :restart do
-  #   run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 'cat #{unicorn_pid}';
-  #    else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi]"
-  # end  
-
-  task :start do 
-    run " cd unicorn_rails -c #{unicorn_conf}-D -E #{rails_env}" # "unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
-  end
-
-  task :stop do
-    run "if [ -f #{unicorn_pid}] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT 'cat #{unicorn_pid}'; fi"
-  end
-end  
+after 'deploy', 'unicorn:reload'    # app IS NOT preloaded
+after 'deploy', 'unicorn:restart'   # app preloaded
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 set :user, "ubuntu"
+set :application, "unicorn_nginx"
 server "ec2-54-200-108-21.us-west-2.compute.amazonaws.com", :app, :web, :db, :primary => true
-ssh_options[:forward_agent] = true
+set :ssh_options, { :forward_agent => true }
